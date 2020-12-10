2551695e878a94ff7d1244abf86a5c3b36130893
Michael Ducharm <ducharmdev@gmail.com>
2020-12-10 18:59:48 -0500

    day 10

--- day10.py
+++ day10.py
@@ -1,6 +1,6 @@
 
 # Setup
-from typing import Counter, Dict, List
+from typing import Dict, List
 import math
 
 test_data_1 = [int(i) for i in open("day10_test_data1.txt", "r").readlines()]
@@ -45,63 +45,25 @@ def problem_1() -> int:
     result: int = math.prod(diff for jolt, diff in diffs.items() if jolt in [1, 3])
     return result
 
-
 problem_1_test()
 
 print(f"Problem 1 answer: {problem_1()}")
 
 # Problem 2
 
-CombinationsPerNumber = Dict[int, int]
-
-def get_combinations_per_number(data: List[int]) -> CombinationsPerNumber:
-    """
-    Returns a dictionary where keys represent each adapter, and values are the number of possible next adapters it can connect to
-    """
-    combination_counts: CombinationsPerNumber = {}
-
-    sorted_data = sorted(data)
-
-    for idx, num in enumerate(sorted_data):
-
-        next_3_nums = sorted_data[idx + 1: idx + 4] if idx > 0 else sorted_data[1:4]
-
-        combination_counts[num] = sum(1 for x in next_3_nums if x - num < 4)
-
-        # if num == sorted_data[-1]:
-        #     combination_counts[num] = num + 3 # last number will be guaranteed to have one path to device's jolt
-
-    return combination_counts
-
-
 def get_all_possible_arrangements(data: List[int]) -> int:
 
-    combination_counts = get_combinations_per_number(data)
+    nums = sorted(data)
 
-    x = sum(i for i in combination_counts.values() if i > 1) + 1
-    return x
+    counts = {
+        0: 1
+    }
 
-    # counts = count_possible_paths(data, 0, combination_counts)
+    for num in nums:
+        counts[num] = sum(counts.get(num - x, 0) for x in range(1, 4))
 
-    # result = math.prod(counts) / len(counts)
-
-    # return result
-
-# def count_possible_paths(data: List[int], count: int, combination_counts: CombinationsPerNumber) -> int:
-
-#     if len(data) == 0:
-#         return count
-
-#     c = count
-
-
-#     for idx, num in enumerate(data):
-#         possible_next_nums = combination_counts.get(num)
-#         c += count_possible_paths(data[idx:], 0, possible_next_nums)
-
-#     return count
-
-
+    return counts[nums[-1]]
+   
 
 def problem_2_test():
     assert get_all_possible_arrangements(test_data_1) == 8
