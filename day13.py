ff1a86fa5d91860e996d9c7d3198bc01d674336b
Michael Ducharm <ducharmdev@gmail.com>
2020-12-13 22:54:22 -0500

    day 13 pt 2 - tests pass, but taking impossibly long...

--- day13.py
+++ day13.py
@@ -1,4 +1,3 @@
-
 from typing import Generator, List
 
 
@@ -50,3 +49,74 @@ problem_1_test()
 
 print(f"Problem 1: {problem_1()}")
 
+# Problem 2
+
+class Bus():
+
+    def __init__(self, id: int, order: int) -> None:
+        self.previous_stop_time = 0
+        self.order = order
+        self.id = id
+
+        self.departure_generator = self._departure_time_gen(0, id)
+        self.next_stop()
+
+    def _departure_time_gen(self, start: int, increment_by: int) -> Generator[int, None, None]:
+        """
+        Returns a generator that increments by a given value indefinitely. 
+        """
+        num = start
+        while True:
+            yield num
+            num += increment_by
+
+    def next_stop(self):
+        self.previous_stop_time = next(self.departure_generator)
+
+def buses_are_sequential(buses: List[Bus]) -> bool:
+    first_bus = buses[0]
+    times = [bus.previous_stop_time - bus.order for bus in buses]
+    return int(sum(times) / len(times)) == first_bus.previous_stop_time
+
+def get_earliest_synced_time(data: List[str]) -> int:
+
+    bus_ids = [int(id) if id != "x" else -1 for id in data[1].split(",")]
+
+    # buses = [Bus(id, idx) for idx, id in enumerate(bus_ids) if id != -1]
+    buses = [Bus(id, idx) for idx, id in enumerate(bus_ids) if id != -1]
+
+    # for each bus, keep going until it gets to the next number divisible  
+
+    for bus in buses:
+        bus.next_stop()
+
+    earliest_time = 0
+
+    while earliest_time == 0:
+
+        first_bus = buses[0]
+
+        for bus in buses[1:]:
+            while ((bus.previous_stop_time - bus.order) % first_bus.id) != 0 or ((first_bus.previous_stop_time + bus.order) > bus.previous_stop_time):
+                bus.next_stop()
+
+            while first_bus.previous_stop_time < (bus.previous_stop_time - bus.order):
+                first_bus.next_stop()
+
+
+        if buses_are_sequential(buses):
+            earliest_time = first_bus.previous_stop_time
+
+    return earliest_time
+
+
+def problem_2_test():
+    assert get_earliest_synced_time(["","17,x,13,19"]) == 3417 
+    assert get_earliest_synced_time(test_data) == 1068781 
+
+def problem_2():
+    return get_earliest_synced_time(data) 
+
+problem_2_test()
+
+print(f"Problem 2: {problem_2()}")
