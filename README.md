51526eeaa155a885f7f3070053093cbae57624bb
Michael Ducharm <ducharmdev@gmail.com>
2020-12-15 22:01:35 -0500

    added simple cli for running solutions & tests

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Advent of Code 2020
 
 You can find this year's problems [here](https://adventofcode.com/2020).
+
+
+```bash
+# Run a specific day
+python .\aoc.py 2 
+
+# Run a specific day's tests
+python .\aoc.py 2 test
+
+# Run all tests
+python -m unittest -v
+```
---
+++ aoc.py
@@ -0,0 +1,30 @@
+import unittest
+import sys
+import importlib
+
+if __name__ == '__main__':
+
+    day = sys.argv[1]
+    day = day if len(day) == 2 else "0" + day
+    if len(sys.argv) == 2:
+
+        module = importlib.import_module(f"day{day}.solutions")
+        
+        problem_1 = getattr(module, "problem_1")
+        problem_2 = getattr(module, "problem_2")
+
+        print(f"Problem 1 answer: {problem_1()}")
+        print(f"Problem 2 answer: {problem_2()}")
+
+    if len(sys.argv) == 3 and sys.argv[2] == "test":
+        module = importlib.import_module(f"day{day}.test")
+
+        test_class = getattr(module, f"Day_{day}_Tests")
+
+        suite = unittest.defaultTestLoader.loadTestsFromTestCase(test_class)
+        unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+
+
+
